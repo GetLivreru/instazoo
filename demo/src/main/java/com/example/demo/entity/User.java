f4c35a7b84f14c59b4399e7f0a179d559e41f9c9
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-06-19 00:52:41 +0500

    create UserDetaiils

--- demo/src/main/java/com/example/demo/entity/User.java
+++ demo/src/main/java/com/example/demo/entity/User.java
@@ -5,55 +5,98 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
 import java.util.*;
 
 @Data
 @Entity
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class User implements UserDetails {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
-    @Column(nullable = false)
-    private String name;
+        @Column(nullable = false)
+        private String name;
 
-    @Column(unique = true, updatable = true)
-    private String username;
+        @Column(unique = true, updatable = true)
+        private String username;
 
-    @Column(nullable = false)
-    private String lastname;
+        @Column(nullable = false)
+        private String lastname;
 
-    @Column(length = 3000)
-    private String password;
+        @Column(length = 3000)
+        private String password;
 
-    @Column(unique = true)
-    private String email;
+        @Column(unique = true)
+        private String email;
 
-    @Column(columnDefinition = "text")
-    private String bio;
+        @Column(columnDefinition = "text")
+        private String bio;
 
-    @ElementCollection(targetClass = ERole.class)
-    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
-    @Enumerated(EnumType.STRING)
-    private Set<ERole> role = new HashSet<>();
+        @ElementCollection(targetClass = ERole.class)
+        @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+        @Enumerated(EnumType.STRING)
+        private Set<ERole> role = new HashSet<>();
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
-    private List<Post> posts = new ArrayList<>();
+        @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
+        private List<Post> posts = new ArrayList<>();
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @Column(updatable = false)
-    private LocalDateTime createdDate;
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        @Column(updatable = false)
+        private LocalDateTime createdDate;
 
-    @Transient
-    private Collection<? extends GrantedAuthorityDefaults> authorities;
-    public User() {
+        @Transient
+        private Collection<? extends GrantedAuthority> authorities;
+        public User() {
 
-    }
+        }
 
-    @PrePersist
-    protected void onCreate() {
-        this.createdDate = LocalDateTime.now();
-    }
+        public User(Long id,
+                    String username,
+                    String email,
+                    String password,
+                    Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+        }
+
+        @PrePersist
+        protected void onCreate() {
+            this.createdDate = LocalDateTime.now();
+        }
+        /*
+            SECURITY
+         */
+        @Override
+        public String getPassword(){
+            return password;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+
+
+
+
+
 }
