7d6acd6faec206374e0a88dee1681a197371962c
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-06-22 17:43:31 +0500

    create request signup

---
+++ demo/src/main/java/com/example/demo/payload/request/SignupRequest.java
@@ -0,0 +1,35 @@
+package com.example.demo.payload.request;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class SignupRequest {
+
+    @Email(message = "It should have email format")
+    @NotBlank(message = "User email is required")
+    // Valid email
+    private String email;
+    @NotEmpty(message = "Please enter your name")
+    private String firstname;
+    @NotEmpty(message = "Please enter your lastname")
+    private String lastname;
+    @NotEmpty(message = "Please enter your username")
+    private String username;
+    @NotEmpty(message = "Password is required")
+    @Size(min = 6)
+    private String password;
+    private String confirmPassword;
+
+
+
+
+
+
+
+
+}
