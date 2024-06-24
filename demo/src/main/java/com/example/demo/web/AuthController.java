fecbfb342cdd08392e153bc6c96779e2b25c9239
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-06-24 17:41:01 +0500

    add BindingResults

---
+++ demo/src/main/java/com/example/demo/web/AuthController.java
@@ -0,0 +1,14 @@
+package com.example.demo.web;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+
+}
