3867121f1dd9d22a50969715444b5d2b18139622
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-06-26 23:57:32 +0500

    qwerty123

--- demo/src/main/java/com/example/demo/annotations/ValidEmail.java
+++ demo/src/main/java/com/example/demo/annotations/ValidEmail.java
@@ -1,20 +1,19 @@
 package com.example.demo.annotations;
 
-
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-import org.hibernate.validator.internal.constraintvalidators.bv.EmailValidator;
+import com.example.demo.validations.EmailValidator;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-@Target({ElementType.TYPE, ElementType.FIELD,ElementType.ANNOTATION_TYPE})
-@Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = EmailValidator.class)
-@Documented
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ValidEmail {
-    String message() default "Invalid Email";
-
+    String message() default "Invalid email";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
-
 }
