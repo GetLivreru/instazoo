6b2a12fc0eb7513015c790bfb6c1fa668e13805e
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-06-28 18:57:32 +0500

    java is !#$%?

--- demo/src/main/java/com/example/demo/annotations/ValidEmail.java
+++ demo/src/main/java/com/example/demo/annotations/ValidEmail.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
 @Constraint(validatedBy = EmailValidator.class)
 @Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ValidEmail {
+public @interface   ValidEmail {
     String message() default "Invalid email";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
