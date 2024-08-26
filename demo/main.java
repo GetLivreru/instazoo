ed6606753925d4407d21cfdbf091d81db687325d
iD1eat <110306539+GetLivreru@users.noreply.github.com>
2024-08-26 20:39:30 +0500

    Update main.java

--- demo/main.java
+++ demo/main.java
@@ -1,1 +1,14 @@
+package com.example.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class InstazooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstazooApplication.class, args);
+	}
+
+}
